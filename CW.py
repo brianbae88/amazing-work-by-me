61eda73195d45a31e97c7d9a03a580064e2f5e1a
김민우 <yjk@gimmin-uui-MacBookPro.local>
2020-07-29 15:18:26 +0900

    Some modifications and comments also check the test cases

--- CW.py
+++ CW.py
@@ -1,20 +1,20 @@
 def isPrime(x):
-	if x == 1 or x < 0:
+	if x == 1 or x < 0:		#Good 
 		return False
-	for i in range (2, x):
+	for i in range (2, x):	#Try to modify x in range 
 		print i 
 		if x % i == 0:
 			return False
-		else:
+		else:					#Perhaps a problem in order 
 			return True
 
 print isPrime(39)
 
 
-def nthPrime(n):
+def nthPrime(n):			#If set numbers are 0, then you need to change the numbers or add by more than 1
 	counter = 0
 	integer = 0
-	while counter < n:
+	while counter < n:			#what happens when n==1? 
 		integer += 1
 		if isPrime(integer) == True:
 			counter += 1
@@ -22,3 +22,14 @@ def nthPrime(n):
 	return integer
 
 print nthPrime(4)
+
+def testisPrime(): 
+	print("Testing isPrime()....")
+	assert (isPrime(-1) == False)
+	assert (isPrime(0) == False)
+	assert (isPrime(677) == True)			#See where the errors occur 
+	assert (isPrime(1013) == True)
+	assert (isPrime(1014)== False)
+	print ("Passed!")
+
+testisPrime()
