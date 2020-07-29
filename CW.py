3c4e3be18155db0b3cd9d30a4b800dfb4c740e55
JY Park <jeyoungp@andrew.cmu.edu>
2020-07-29 15:14:18 +0900

    I addded two test case functions and some comments

--- CW.py
+++ CW.py
@@ -1,5 +1,5 @@
 def isPrime(x):
-	if x == 1 or x < 0:
+	if x == 1 or x < 0:                          # this is where you check if x is sma;ller than 1 
 		return False
 	for i in range (2, x):
 		print i 
@@ -22,3 +22,21 @@ def nthPrime(n):
 	return integer
 
 print nthPrime(4)
+
+
+
+
+def testIsPrime():
+    print("Testing isPrime()...")
+    assert(isPrime(-1) == False)  # negative
+    assert(isPrime(0) == False)   # zero
+    assert(isPrime(1) == False)   # 1 is quite the special case
+    assert(isPrime(2) == True)    # 2, only even prime
+    assert(isPrime(3) == True)    # 3, smallest odd prime
+    assert(isPrime(4) == False)   # 4, smallest even non-prime
+    assert(isPrime(9) == False)   # 9, perfect square of odd prime
+    assert(isPrime(987) == False) # somewhat larger non-prime
+    assert(isPrime(997) == True)  # somewhat larger prime
+    print("Passed!")
+
+testIsPrime()
